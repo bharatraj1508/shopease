025a934a18e1d05583fe8a8fd09b54a984214896
Bharat Raj Verma <bharat.raj1508@gmail.com>
2024-11-20 15:23:48 +0530

    setup the db

---
+++ index.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(cors);
+app.use(bodyParser.json);
+
+mongoose.connect(process.env.MONGOURI);
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to Mongo instance");
+});
+
+mongoose.connection.on("error", (error) => [
+  console.log("Error connecting to mongo instance", error),
+]);
+
+app.get("/", (req, res) => {
+  res.status(200).send({ message: "Server is online" });
+});
+
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+});
